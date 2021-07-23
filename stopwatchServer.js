2d0256e8bb1541b164df02a4bdfe21a498a99f57
SaggyGILF <47832424+SaggyGILF@users.noreply.github.com>
2021-07-23 13:27:07 -0400

    Fixed routes and hostname for server

--- stopwatchServer.js
+++ stopwatchServer.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const PORT = 80;
-const hostName = '192.168.1.1';
+const hostname = '0.0.0.0';
 
 
 // Main routes to serve static files
 app.use(express.static('public'));
-app.use('/JS', express.static(__dirname + 'public/CSS'));
+app.use('/CSS', express.static(__dirname + 'public/CSS'));
 app.use('/JS', express.static(__dirname + 'public/JS'));
 
 // Views Config
@@ -23,4 +23,4 @@ app.get('', (req, res) => {
 });
 
 // Listening...
-app.listen(PORT, console.info(`Listening on port ${PORT}...`));
+app.listen(PORT, hostname, console.info(`Listening on port ${PORT}...`));
